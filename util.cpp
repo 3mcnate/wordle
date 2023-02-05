09a1dea8b785da8fb1bc2f132351e83697a95a76
Tanya Sankhla <sankhlaa@usc.edu>
2023-02-04 17:31:43 -0800

    final push

--- util.cpp
+++ util.cpp
@@ -81,6 +81,11 @@ bool checkWord(string input, string word)
         } else if ((input[i] == correctWord[0]) || (input[i] == correctWord[1]) || (input[i] == correctWord[2]) || (input[i] == correctWord[3]) || (input[i] == correctWord[4])){
             // print that index in yellow 
             cout << "\x1b[33m[" << input[i] << "]\x1b[0m";
+            for(int j = 0; j < 5; j++){
+                if(input[i] == correctWord[j]){
+                    correctWord[j] = '\0';
+                }
+            }
 
         } else {
             // print in red
