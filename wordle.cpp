b38b82c0ad54b9e091df3eca211a8612ef1eb2fc
Nick Lee-Roller <leerolle@usc.edu>
2023-02-04 16:07:16 -0800

    Update wordle.cpp

--- wordle.cpp
+++ wordle.cpp
@@ -47,8 +47,8 @@ int main(int argc, char **argv)
     bool p2Won = false;
     bool p1Won = false;
     string p1, p2, g1, g2, guessCheck;
-    int p1GuessRemain = 5;
-    int p2GuessRemain = 5;
+    int p1GuessRemain = 7;
+    int p2GuessRemain = 7;
 
     cout << "Player 1, enter your name: " << endl;
     cin >> p1;
@@ -57,56 +57,27 @@ int main(int argc, char **argv)
 
     cout << endl;
 
-    while (p1GuessRemain > 0 && p2GuessRemain > 0 && p1Won == false && p2Won == false)
-    {
-        if (p1GuessRemain < 7)
-        {
+    while(p1GuessRemain > 0 && p2GuessRemain > 0 && p1Won == false && p2Won == false){
+        if(p1GuessRemain < 7){
             cout << "Dont scroll up." << endl;
         }
         cout << p1 << ", make your guess: " << endl;
         cin >> g1;
-        if (g1.size() != 5 || !isWordValid(validWords, g1))
-        { 
+        if(g1.size() != 5){ // || !isWord(g1)
             cout << "Please enter a word that is 5 letters long: " << endl;
             cin >> g1;
-            while (g1.size() != 5 || !isWordValid(validWords, g1))
-            {
+            while(g1.size() != 5){ // || !isWord(g1)
                 cout << "Please enter a word that is 5 letters long: " << endl;
                 cin >> g1;
             }
         }
-        // guessCheck = makeGuess(g1);
-        for (int i = 0; i < 5; i++)
-        {
-            if (guessCheck[i] == '0')
-            {
-                // make letter grey
-                cout << "[ " << g1[i] << " ]" << endl;
-            }
-            else if (guessCheck[i] == '1')
-            {
-                // make letter yellow
-                cout << "[ " << g1[i] << " ]" << endl;
-            }
-            else if (guessCheck[i] == '2')
-            {
-                // make letter green
-                cout << "[ " << g1[i] << " ]" << endl;
-            }
-        }
-
-        if (guessCheck[0] == '2' && guessCheck[1] == '2' && guessCheck[2] == '2' && guessCheck[3] == '2' && guessCheck[4] == '2')
-        {
-            cout << p1 << " Guessed the correct Word! Last chance " << p2 << "!" << endl;
-            p1Won = true;
-        }
+        p1Won = checkGuess(g1, word);
 
         cout << "Press Enter to Continue" << endl;
         cin.ignore();
         cin.get();
-        
-        for (int k = 0; k < 100; k++)
-        { // clear the terminal at end of turn
+
+        for(int k = 0; k < 100; k++){ //clear the terminal at end of turn
             cout << endl;
         }
 
@@ -114,54 +85,27 @@ int main(int argc, char **argv)
         cout << "________________________________________________________________" << endl;
         cout << endl;
 
-        // player 2's turn
+        //player 2's turn
         cout << "Dont scroll up." << endl;
+        
 
         cout << p2 << ", make your guess: " << endl;
         cin >> g2;
-        if (g2.size() != 5 || !isWordValid(validWords, g2))
-        { 
+        if(g2.size() != 5){ // || !isWord(g2)
             cout << "Please enter a word that is 5 letters long: " << endl;
             cin >> g2;
-            while (g2.size() != 5 || !isWordValid(validWords, g2))
-            { 
+            while(g2.size() != 5){ // || !isWord(g2)
                 cout << "Please enter a word that is 5 letters long: " << endl;
                 cin >> g2;
             }
         }
-        // guessCheck = makeGuess(g1);
-        for (int i = 0; i < 5; i++)
-        {
-            if (guessCheck[i] == '0')
-            {
-                // make letter grey
-                cout << "[ " << g2[i] << " ]" << endl;
-            }
-            else if (guessCheck[i] == '1')
-            {
-                // make letter yellow
-                cout << "[ " << g2[i] << " ]" << endl;
-            }
-            else if (guessCheck[i] == '2')
-            {
-                // make letter green
-                cout << "[ " << g2[i] << " ]" << endl;
-            }
-        }
-
-        if (guessCheck[0] == '2' && guessCheck[1] == '2' && guessCheck[2] == '2' && guessCheck[3] == '2' && guessCheck[4] == '2')
-        {
-            cout << p2 << " Guessed the correct word!" << endl;
-            p2Won = true;
-            break;
-        }
+        p2Won = checkGuess(g2, word);
 
         cout << "Press Enter to Continue" << endl;
         cin.ignore();
         cin.get();
 
-        for (int k = 0; k < 100; k++)
-        { // clear the terminal at end of turn
+        for(int k = 0; k < 100; k++){ //clear the terminal at end of turn
             cout << endl;
         }
 
@@ -173,20 +117,13 @@ int main(int argc, char **argv)
         p2GuessRemain -= 1;
     }
 
-    if (p1Won == false && p2Won == false)
-    {
+    if(p1Won == false && p2Won == false){
         cout << "Nobody Wins!" << endl;
-    }
-    else if (p1Won == true && p2Won == true)
-    {
+    }else if(p1Won == true && p2Won == true){
         cout << "Tie game." << endl;
-    }
-    else if (p1Won == true && p2Won == false)
-    {
+    }else if(p1Won == true && p2Won == false){
         cout << p1 << " Won! Better luck next time " << p2 << "." << endl;
-    }
-    else
-    {
+    }else{
         cout << p2 << " Won! Better luck next time " << p1 << "." << endl;
     }
 
